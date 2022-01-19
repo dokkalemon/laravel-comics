e44f49ecd96167da7f4ad08e48be7b8b08201d85
dokkalemon <dottori.roberto@gmail.com>
2022-01-19 15:39:13 +0100

    add mix

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,6 +6,8 @@
 
         <title>Laravel</title>
 
+        <link rel="stylesheet" href="../../p">
+
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
 
