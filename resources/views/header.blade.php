1ddef333bfd2a5d177a78721f01550ce34453720
dokkalemon <dottori.roberto@gmail.com>
2022-01-19 16:25:03 +0100

    add Header

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,3 +1,25 @@
 
+<header class="full-width">
+    <div class="header container px-10">
+        <div class="logo">
+            <img src="{{ asset('images/dc-logo.png') }}" alt="">
+        </div>
 
-qui header
+        <nav>
+            <ul>
+                <li
+                @if (Request::route()->getName() === 'home') class="active" @endif
+                >
+                    <a href="/">COMICS</a>
+                </li>
+                <li >
+                    @if (Request::route()->getName() === 'news') class="active" @endif
+                    <a href="/news">NEWS</a>
+                </li>
+            </ul>
+        </nav>
+
+        
+
+    </div>
+</header>
