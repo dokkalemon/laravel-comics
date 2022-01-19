ab4fbed8e565a9b8f2134093652acb78acb15400
dokkalemon <dottori.roberto@gmail.com>
2022-01-19 17:43:18 +0100

    add home main section

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,14 +5,19 @@
 <main class="full-width">
     <div class="comics-container container px-10">
         
+        @foreach ($comics as $comic)
         <div class="card px-10">
             <div class="card-container">
-                <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX" alt="">
-                <h4></h4>
+                <img src="{{$comic['thumb']}}" alt="">
+                <h4>{{$comic['series']}}</h4>
             </div>
         </div>
+        @endforeach
 
-       
+        <button class="primary-button home_button">LOAD MORE</button>
+        
         
     </div>
 </main>
+
+@endsection
