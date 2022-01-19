32361af2af51cf14f670b49c4d7f2a10f06109da
dokkalemon <dottori.roberto@gmail.com>
2022-01-19 18:01:46 +0100

    add news section

--- resources/views/news.blade.php
+++ resources/views/news.blade.php
@@ -1,3 +1,17 @@
 @extends('layout.main')
 
 @section('main')
+
+<div class="news-main full-width">
+    <div class="news-container container px-10">
+        <h1>News</h1>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis eligendi porro doloremque, dolor at quae quam repellat ducimus inventore incidunt dolores aliquam veritatis nesciunt labore, dolorem repellendus nihil possimus ab?</p>
+
+
+
+
+    </div>
+</div>
+
+@endsection
